c231dd48b43e800c0f4815cd46df0773e8a6b23a
Otor001 <otor4real2007@gmail.com>
2024-07-15 00:11:04 +0100

    Update on Module 13A Task 6

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const myFavLanguage = "JavaScript"
+//const myFavLanguage = "JavaScript"
 
-const date = 6
-const name = 'friday 5th june 2024'
-const sentence = 'Todays date is ' + name +'. '+'We are in the 20th weeks of the year, the month is '+date+'th of the year'
+//const date = 6
+//const name = 'friday 5th june 2024'
+//const sentence = 'Todays date is ' + name +'. '+'We are in the 20th weeks of the year, the month is '+date+'th of the year'
 
-console.log(sentence)
+
+//const isBritish = true
+//const isNight = false
+
+//const userDetails = null
+
+//const addition = 40+2 
+
+//const compare = 4 > 3   
+
+//const userName = 'Success'
+//const age = 20
+
+//const logic = age ===12 || userName === 'victory'  
+
+const age = 50
+
+if(age>=32 ) {
+    console.log('You are eligible to vote')
+} else {
+    console.log('You are too young to vote')
+}
+  
 
