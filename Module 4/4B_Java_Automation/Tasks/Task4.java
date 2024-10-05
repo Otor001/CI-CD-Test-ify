e57acb74a4bf0b0c1bac663ba3d6cb3fb92611de
Otor001 <otor4real2007@gmail.com>
2024-10-05 22:50:15 +0100

    Task4.java

---
+++ Module 4/4B_Java_Automation/Tasks/Task4.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task4 {
+    public static void main(String[] args) {
+        // Storing the radius of the circle
+        double radius = 12.7;
+
+        // Calculating the area using the formula Task11 = π * r^2
+        double area = Math.PI * Math.pow(radius, 2);
+
+        // Printing the result with the unit "metres"
+        System.out.println("The area of the circle is " + area + " square metres.");
+    }
+}
