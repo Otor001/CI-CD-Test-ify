b52f4c06a08b38a354efd3d492c5112634e3d7a2
Otor001 <otor4real2007@gmail.com>
2024-10-07 18:31:54 +0100

    Task11/

---
+++ Module 4/4B_Java_Automation/Tasks/Task11/ClassB.java
@@ -0,0 +1,11 @@
+package Tasks.Task11;
+
+public class ClassB {
+
+    public static void main(String[] args) {
+        ClassA classA = new ClassA();
+        String name = classA.getName("Emmanuel");
+        System.out.println(name);
+    }
+
+}
