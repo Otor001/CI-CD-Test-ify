e7660a05859e3b56eaf45e272453a8f69d7b6301
Otor001 <otor4real2007@gmail.com>
2024-10-07 18:43:29 +0100

    Task12/

---
+++ Module 4/4B_Java_Automation/Tasks/Task12/ClassA.java
@@ -0,0 +1,8 @@
+package Tasks.Task12;
+
+public class ClassA {
+    // Public method that can be accessed anywhere in the project
+    public void MidName() {
+        System.out.println("Obriku");
+    }
+}
